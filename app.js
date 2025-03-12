4eb9a25e41cd568e07ff7cba5ef104619f18954a
Mario D. Gonzalez <mario.gonzalez@leapmetrics.io>
2025-03-12 02:15:57 -0500

    Adding more fields

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ document.getElementById('patientForm').addEventListener('submit', function(event
     };
 
     // Enviar los datos usando Fetch API
-    fetch('https://hl7-fhir-ehr.onrender.com/patient/', {
+    fetch('https://hl7-fhir-ehr.onrender.com/patient', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
