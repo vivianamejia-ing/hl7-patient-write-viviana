f1e2fdebc381aad1af42497bb1df9ce2578c4509
Mario D. Gonzalez <mario.gonzalez@leapmetrics.io>
2025-03-11 22:34:01 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+document.getElementById('patientForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Obtener los valores del formulario
+    const name = document.getElementById('name').value;
+    const familyName = document.getElementById('familyName').value;
+    const gender = document.getElementById('gender').value;
+    const birthDate = document.getElementById('birthDate').value;
+
+    // Crear el objeto Patient en formato FHIR
+    const patient = {
+        resourceType: "Patient",
+        name: [{
+            use: "official",
+            given: [name],
+            family: familyName
+        }],
+        gender: gender,
+        birthDate: birthDate
+    };
+
+    // Enviar los datos usando Fetch API
+    fetch('https://hl7-fhir-ehr.onrender.com/patient/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(patient)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        alert('Paciente creado exitosamente!');
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('Hubo un error al crear el paciente.');
+    });
+});
